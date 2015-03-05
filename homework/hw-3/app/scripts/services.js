4dcdb122615305fa2ffebced547c4fbb0f6ffc94
Alex Hidalgo <aj1hidalgo@gmail.com>
2015-03-04 19:26:13 -0600

    Added fixes to hw2 and hw3 lib-sass. Added ctrl and service fixes to hw3

--- homework/hw-3/app/scripts/services.js
+++ homework/hw-3/app/scripts/services.js
@@ -2,28 +2,46 @@ angular.module('app.services', [])
 .factory('RandomArrayValue', function() {
 	// Create a way to get a random integer based on that arrays length
 
-	return function getRandomInt(myArray) {
+	return function (myArray) {
 	   var randomArrayPos = Math.floor(Math.random() * (myArray.length - 0)) + 0;
-	   // console.log(myArray[randomArrayPos]);
-	   return randomArrayPos;
+
+	   console.log(randomArrayPos);
 	};
 
 })
-.factory('Pair', function() {
-	return function randomPair(studentList) {
+.factory('Pair', function () {
+	//Create random pair of studentList in Ctrl
+	return function (studentList) {
+
+		var randomArrayPos = function() {
+				return Math.floor(Math.random() * (studentList.length - 0)) + 0
+			};
+
+		var randomPosition = function () {
+				return randomArrayPos(studentList);
+			};
 
 		for(i=0; i < studentList.length; i+2) {
+			// console.log(studentList.length);
 			var pairedList = [];
-			var randomArrayPos = Math.floor(Math.random() * (studentList.length - 0)) + 0;
-			myPair = [];
-			myPair += studentList.splice(randomArrayPos(), 1) + " ";
-			myPair += studentList.splice(randomArrayPos(), 1);
+			var myPair = [];
 
+			myPair += studentList.splice(randomPosition(), 1) + " ";
+			// myPair += studentList.splice(randomPosition(), 1) + " ";
+			// myPair += studentList.splice(randomPosition(), 1) + " ";
+			// myPair += studentList.splice(randomPosition(), 1) + " ";
+			// myPair += studentList.splice(randomPosition(), 1) + " ";
+			// myPair += studentList.splice(randomPosition(), 1) + " ";
+			// myPair += studentList.splice(randomPosition(), 1) + " ";
+			// myPair += studentList.splice(randomPosition(), 1);
+			//
 			pairedList.push(myPair);
+			console.log(pairedList);
 		}
-		console.log(pairedList);
-		// return pairedList;
+
+
 	};
+
 })
 .factory('SplitString', function() {
 	return function sliceCounter(string) {
